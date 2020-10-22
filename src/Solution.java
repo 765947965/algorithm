f172679bd72d4899efb5d02e331d141793fa1e3e
765947965 <765947965@qq.com>
2020-10-22 09:12:38 +0800

    ç¬¬äºæ¬æäº¤

--- src/Solution.java
+++ src/Solution.java
@@ -8,7 +8,7 @@ public class Solution {
 
 
     public static void main(String[] args) {
-        String a = "afdfewrewvcxwe329423fdsfsddf";
+        String a = "afdfewrewvcxwe329423fdsfsddf111111";
         long time = System.currentTimeMillis();
         //System.out.println(generateParenthesis2(15).toString());
 //        System.out.println(divide(-2147483648,
