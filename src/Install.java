5ef97de482e486be5780ec321fe65abbb5a0482c
765947965 <765947965@qq.com>
2020-11-02 09:26:48 +0800

    升级版本

--- src/Install.java
+++ src/Install.java
@@ -12,14 +12,14 @@ public class Install {
     private static void installARC() {
         String[] ips = new String[]{"192.168.72.26", "192.168.72.30", "192.168.72.35", "192.168.4.241",
                 "192.168.72.28", "192.168.72.33", "192.168.72.32", "192.168.72.34", "192.168.4.214", "192.168.72.29",
-                "192.168.72.31",  "192.168.4.231"};
-        String path = "C:\\Users\\76594\\Desktop\\1\\eastArc\\release\\eastArc_125.apk";
+                "192.168.72.31"};
+        String path = "C:\\Users\\76594\\Desktop\\1\\eastArc\\release\\eastArc_126.apk";
         installValue(ips, path);
     }
 
     private static void installBD() {
         String[] ips = new String[]{"192.168.160.91", "192.168.4.106", "192.168.4.53"};
-        String path = "C:\\Users\\76594\\Desktop\\1\\eastBaidu\\release\\eastBaidu_125.apk";
+        String path = "C:\\Users\\76594\\Desktop\\1\\eastBaidu\\release\\eastBaidu_126.apk";
         installValue(ips, path);
     }
 
